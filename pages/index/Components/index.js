ab75490c4fd43ef356665ad75afa35781bf8903f
yang <yueyangyu42@gmail.com>
2020-01-29 15:06:43 +0800

    Remove commented out code

--- pages/index/Components/index.js
+++ pages/index/Components/index.js
@@ -1,6 +1,5 @@
 import CodeEditor from "./CodeEditor";
 import CodePreviewer from "./CodePreviewer";
-import MarkdownEditor from "./MarkdownEditor";
 import MarkdownPreviewer from "./MarkdownPreviewer";
 import PlaintextEditor from "./PlaintextEditor";
 import PlaintextPreviewer from "./PlaintextPreviewer";
@@ -8,7 +7,6 @@ import PlaintextPreviewer from "./PlaintextPreviewer";
 module.exports = {
   CodeEditor: CodeEditor,
   CodePreviewer: CodePreviewer,
-  MarkdownEditor: MarkdownEditor,
   MarkdownPreviewer: MarkdownPreviewer,
   PlaintextEditor: PlaintextEditor,
   PlaintextPreviewer: PlaintextPreviewer
